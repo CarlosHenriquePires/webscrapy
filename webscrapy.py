0d9ffd1e4a7c2ead95359262234c81fe5185a941
carlzenriques <carluxhenrique@gmail.com>
2019-07-04 09:49:17 -0300

    Ajeitando o codigo para valores vazios no site

--- webscrapy.py
+++ webscrapy.py
@@ -2,32 +2,32 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import pandas as pd
 
-titulos=[]
-detalhes_=[]
-precos_=[]
-locais=[]
+todos=[]
 page = 1
-while(page < 3):
-    url = urlopen("https://rj.olx.com.br/autos-e-pecas?o="+str(page))
-    print(url)
+while (page < 3):
+    url = urlopen("https://rn.olx.com.br/autos-e-pecas?o="+str(page))
     soup = BeautifulSoup(url.read(),"html.parser")
-    titulo = soup.findAll("h2", {"class": "OLXad-list-title"})
+    titulo= soup.findAll("h2", {"class": "OLXad-list-title"})
     detalhes=soup.findAll("p", {"class": "text detail-specific"})
     preco=soup.findAll("p", {"class": "OLXad-list-price"})
     local=soup.findAll("p", {"class": "text detail-region"})
-    for title in titulo:
-        text = title.getText()
-        text = text.replace('\n','\t\t\t\t\t\t\t')
-        titulos.append(text)
+    page+=1
+for title in titulo:
+    title = title.getText()
+    title = title.replace("\n", "")
+    title = title.replace("\t", "")
     for detail in detalhes:
-        detalhes_.append(detail.getText())
-    for price in preco:
-        precos_.append(price.getText())
-    for origen in local:
-        locais.append(origen.getText())
-    page = page + 1
-
-test = list(zip(titulos,detalhes_,precos_,locais))
-df = pd.DataFrame(test)
-df.to_csv("webscrapy.csv")
-
+        detail = detail.getText()
+        detail = detail.replace("\n", "")
+        detail = detail.replace("\t","")
+        for price in preco:
+            price = price.getText()
+            price = price.replace("\n", "")
+            price = price.replace("\t","")
+            for origen in local:
+                origen = origen.getText()
+                origen = origen.replace("\n", "")
+                origen = origen.replace("\t","")
+    todos.append([title,detail,price,origen])
+df = pd.DataFrame(todos,columns=('Titulo','Detalhes','Preço','Local'))
+df.to_csv('webscraping.csv')
